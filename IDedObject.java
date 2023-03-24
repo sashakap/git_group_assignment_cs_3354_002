84a3a6ca834ef709a837769fb41a10bcfc391ca7
maviyay <80171342+maviyay@users.noreply.github.com>
2023-03-24 14:21:09 -0500

    IDedObject

---
+++ IDedObject.java
@@ -0,0 +1,10 @@
+/** IDed Interface
+ * @author Sasha Kaplan
+ * @author CS 3345.003
+ * @since 02-22-2023
+ * @version 1.0
+*/
+interface IDedObject {
+	public int getID();                 		//Returns the ID of the object
+	public void printID();         				//Prints the details of the ID
+}
